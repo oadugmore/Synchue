465320c040266ba9413a796f76c9c9f334e43b0f
Owen Dugmore <oadugmore@outlook.com>
2018-10-17 19:32:12 -0700

    init

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    private static Controller current = null;
+    private bool pressed = false;
+    private int pointerId = -1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (current == null)
+            current = this;
+        else
+        {
+            Debug.LogError("Only one instance of Controller is allowed.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static bool screenIsPressed()
+    {
+        return current.pressed;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (pressed) return;
+        pointerId = eventData.pointerId;
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.pointerId == pointerId)
+            pressed = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+	bool lastMsg = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log("Player starting");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Controller.screenIsPressed() != lastMsg)
+		{
+			string msg = lastMsg ? "PointerUp" : "PointerDown";
+			Debug.Log(msg);
+			lastMsg = !lastMsg;
+		}
+	}
+}
