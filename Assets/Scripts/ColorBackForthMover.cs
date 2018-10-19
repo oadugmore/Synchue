7acf5290e0bb9ea9750002eda2f4bcb3619c2419
Owen Dugmore <oadugmore@outlook.com>
2018-10-19 01:09:38 -0700

    Added tetrahedron and started mover

---
+++ Assets/Scripts/ColorBackForthMover.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorBackForthMover : MonoBehaviour
+{
+
+    public float controlDistance = 15f;
+    public Direction movementDirection = Direction.UpDown;
+    public float movementSpeed = 5f;
+    public float movementDistance = 4f;
+    //public static Material blueMaterial;
+    //public static Material orangeMaterial;
+    public InteractColor color = InteractColor.Blue;
+
+    private bool movingForward;
+    private Rigidbody m_rigidbody;
+    private Transform cameraTransform;
+	private Vector3 forwardDirection;
+	private Vector3 backwardDirection;
+	private Vector3 initialPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        movingForward = true;
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_rigidbody.isKinematic = true;
+        cameraTransform = Camera.main.transform;
+		initialPosition = transform.position;
+		SetMovementDirectionVectors();
+    }
+
+	public void SetMovementDirectionVectors()
+	{
+		switch (movementDirection)
+		{
+			case (Direction.DownUp):
+				forwardDirection = Vector3.down;
+				backwardDirection = Vector3.up;
+				break;
+			case (Direction.UpDown):
+				forwardDirection = Vector3.up;
+				backwardDirection = Vector3.down;
+				break;
+			case (Direction.LeftRight):
+				forwardDirection = Vector3.left;
+				backwardDirection = Vector3.right;
+				break;
+			case (Direction.RightLeft):
+				forwardDirection = Vector3.right;
+				backwardDirection = Vector3.left;
+				break;
+			default:
+				throw new System.NotImplementedException();
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (color == InteractColor.Blue && Controller.GetBlueButtonDown()
+        || color == InteractColor.Orange && Controller.GetOrangeButtonDown())
+        {
+            if (Vector3.Distance(cameraTransform.position, transform.position) < controlDistance)
+            {
+                Move();
+            }
+        }
+    }
+
+    void Move()
+    {
+		if (ShouldSwitchDirection())
+		{
+			movingForward = !movingForward;
+			m_rigidbody.velocity = Vector3.zero;
+		}
+		if (m_rigidbody.velocity.magnitude < movementSpeed)
+		{
+			if (movingForward)
+				m_rigidbody.AddForce(forwardDirection * movementSpeed);
+			else
+				m_rigidbody.AddForce(backwardDirection * movementSpeed);
+		}
+    }
+
+	private bool ShouldSwitchDirection()
+	{
+		Vector3 difference = initialPosition - transform.position;
+		switch (movementDirection)
+		{
+			case Direction.DownUp:
+				return (movingForward && difference.y > movementDistance) || (!movingForward && difference.y < 0);
+			case Direction.UpDown:
+				return (movingForward && difference.y < -movementDistance) || (!movingForward && difference.y > 0);
+			case Direction.LeftRight:
+				return (movingForward && difference.x > movementDistance) || (!movingForward && difference.x < 0);
+			case Direction.RightLeft:
+				return (movingForward && difference.x < -movementDistance) || (!movingForward && difference.x > 0);
+			default:
+				throw new System.NotImplementedException();
+		}
+	}
+
+}
+
+public enum Direction
+{
+    UpDown, DownUp, LeftRight, RightLeft
+}
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -2,31 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMover : MonoBehaviour {
+public class PlayerMover : MonoBehaviour
+{
 
+	public float maxSpeed = 5f;
+	public float speedIncrement = 10f;
+	public float minSpeed = 0.5f;
+	//public PhysicMaterial movingMaterial;
+	//public PhysicMaterial stoppedMaterial;
 
-	// Use this for initialization
-	void Start () 
+    private InteractColor playerColor;
+    private Rigidbody playerRigidbody;
+	private SphereCollider sphereCollider;
+	private BoxCollider boxCollider;
+	private bool moving = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Player starting");
+        playerRigidbody = GetComponent<Rigidbody>();
+		sphereCollider = GetComponentInChildren<SphereCollider>();
+		boxCollider = GetComponent<BoxCollider>();
+		playerColor = InteractColor.Blue;
+    }
+
+    // bool lastBlueMsg = false;
+    // bool lastOrangeMsg = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (playerColor == InteractColor.Blue && Controller.GetBlueButtonDown()
+		|| playerColor == InteractColor.Orange && Controller.GetOrangeButtonDown())
+        {
+			//if (!moving)
+				Move();
+        }
+        else //if (moving)
+        {
+            Stop();
+        }
+    }
+
+	private void Move()
 	{
-		Debug.Log("Player starting");
+		moving = true;
+		//playerCollider.material = movingMaterial;
+		//sphereCollider.isTrigger = false;
+		//boxCollider.isTrigger = true;
+		if (playerRigidbody.velocity.x < maxSpeed)
+		{
+		playerRigidbody.AddForce(Vector3.right * speedIncrement);
+
+		Debug.Log("Added force.");
+		}
 	}
-	
-	bool lastBlueMsg = false;
-	bool lastOrangeMsg = false;
-	// Update is called once per frame
-	void Update () 
+
+	private void Stop()
 	{
-		if (Controller.GetBlueButtonDown() != lastBlueMsg)
+		moving = false;
+		//sphereCollider.material = stoppedMaterial;
+		//sphereCollider.isTrigger = true;
+		//boxCollider.isTrigger = false;
+		//
+		if (playerRigidbody.velocity.x > minSpeed)
 		{
-			string msg = lastBlueMsg ? "blue up" : "blue down";
-			Debug.Log(msg);
-			lastBlueMsg = !lastBlueMsg;
+			playerRigidbody.AddForce(Vector3.left * speedIncrement);
+			Debug.Log("Stopping...");
 		}
-		if (Controller.GetOrangeButtonDown() != lastOrangeMsg)
+		else if (playerRigidbody.velocity.x != 0)
 		{
-			string msg = lastOrangeMsg ? "orange up" : "orange down";
-			Debug.Log(msg);
-			lastOrangeMsg = !lastOrangeMsg;
+			playerRigidbody.velocity = Vector3.zero;
+			
+			Debug.Log("Stopped.");
 		}
+		
 	}
+
+	public void ChangeColor(InteractColor newColor)
+	{
+		playerColor = newColor;
+	}
+
 }
