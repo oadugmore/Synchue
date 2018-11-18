8d52526b77e5c114f2976bcef4f1e32ed43f381e
Owen Dugmore <oadugmore@outlook.com>
2018-11-17 23:52:52 -0800

    Created pause menu and primitive main menu

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void LoadLevel(int levelNumber)
+	{
+		string sceneName = "Level" + levelNumber;
+		SceneManager.LoadScene(sceneName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour 
+{
+	bool paused = false;
+	Animator menuAnim;
+
+	const string menuSceneName = "Menu";
+
+	// Use this for initialization
+	void Start () 
+	{
+		menuAnim = GetComponentInChildren<Animator>();
+	}
+
+	public void PauseButtonPressed()
+	{
+		paused = !paused;
+		menuAnim.SetBool("Paused", paused);
+		if (paused)
+		{
+			Time.timeScale = 0f;
+			
+		}
+		else
+		{
+			Time.timeScale = 1f;
+		}
+	}
+
+	public void RestartButtonPressed()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void MenuButtonPressed()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(menuSceneName);
+	}
+	
+}
