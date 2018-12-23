a04246a716ee6acb9cee06d0851b23ce25d70629
Owen Dugmore <oadugmore@outlook.com>
2018-12-23 00:05:47 -0800

    added basic conveyor script and found a "curved ground" package

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+
+    public Vector3 force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("stay");
+        Rigidbody r;
+        if (r=collider.gameObject.GetComponentInChildren<Rigidbody>())
+        {
+            Debug.Log("force");
+            r.AddForce(force);
+        }
+    }
+}
