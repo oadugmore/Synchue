90d1c0b8dc0c5f6576bef31d2851805a0847a742
Owen Dugmore <oadugmore@outlook.com>
2019-04-06 23:02:14 -0700

    Fixed CSinusoidalMovementObject

--- Assets/Scripts/CSinusoidalMovementObject.cs
+++ Assets/Scripts/CSinusoidalMovementObject.cs
@@ -8,9 +8,6 @@ public class CSinusoidalMovementObject : CCyclePathingObject
     // [SerializeField][Range(0f, 1f)]
     // private float cycleOffset = 0f;
 
-    public float angle = 0;
-    public float nextFraction = 0f;
-
     Rigidbody movementObject;
     //private float radius;
 
@@ -28,26 +25,22 @@ public class CSinusoidalMovementObject : CCyclePathingObject
         // if (localCyclePos >= 1f) localCyclePos -= 1f;
         int nextIndex = NextNode(cyclePos);
         int previousIndex = PreviousNode(nextIndex);
-        CSinusoidalMovementNode previous = (CSinusoidalMovementNode)nodes[previousIndex];
-        CSinusoidalMovementNode next = (CSinusoidalMovementNode)nodes[nextIndex];
+        var previous = (CSinusoidalMovementNode)nodes[previousIndex];
+        var next = (CSinusoidalMovementNode)nodes[nextIndex];
 
         float nextCyclePos = next.TargetCyclePosition();
         float previousCyclePos = previous.TargetCyclePosition();
         float nextAngle = next.Angle();
         float previousAngle = previous.Angle();
-        if (nextAngle < 0f && previousAngle > 0f) nextAngle += 360f;
-        if (nextAngle > 0f && previousAngle < 0f) previousAngle += 360f;
+        if (nextAngle < 0f && previousAngle > 0f && !next.RotateClockwise()) nextAngle += 360f;
+        if (nextAngle > 0f && previousAngle < 0f && next.RotateClockwise()) previousAngle += 360f;
         while (nextCyclePos < previousCyclePos) nextCyclePos++;
 
         float fraction = Mathf.Abs(cyclePos - previousCyclePos) / (nextCyclePos - previousCyclePos);
         float newAngle = Mathf.Lerp(previousAngle, nextAngle, fraction);
-        angle = newAngle;
-        nextFraction = fraction;
         float newRadius = Mathf.Lerp(previous.Radius(), next.Radius(), fraction);
 
-        //float input = pi2 * localCyclePos;
-        float angleRadians = Mathf.PI * newAngle / 180f;
-        if (!next.RotateClockwise()) angleRadians *= -1f;
+        float angleRadians = Mathf.Deg2Rad * newAngle;
         float h = Mathf.Cos(angleRadians) * newRadius;
         float v = Mathf.Sin(angleRadians) * newRadius;
         Vector3 destination = transform.TransformPoint(h, v, 0);
