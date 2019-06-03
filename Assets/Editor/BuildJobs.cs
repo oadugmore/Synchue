cd8203de539e483dcd2e90cb0c7585169596e097
Owen Dugmore <oadugmore@outlook.com>
2019-06-02 23:57:07 -0700

    Added Build Job for Jenkins CI

---
+++ Assets/Editor/BuildJobs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildJobs
+{
+
+    private static BuildPlayerOptions CreatePlayerOptions(BuildTarget target)
+    {
+        string buildName = "Builds/";
+        switch (target)
+        {
+            case BuildTarget.Android:
+                buildName += "PlatformerAndroid";
+                break;
+            case BuildTarget.iOS:
+                buildName += "PlatformeriOS";
+                break;
+            default:
+                buildName += "Platformer";
+                break;
+        }
+        var options = new BuildPlayerOptions
+        {
+            scenes = FindEnabledEditorScenes(),
+            target = target,
+            options = BuildOptions.None,
+            locationPathName = buildName
+        };
+        return options;
+    }
+
+
+    [MenuItem("Jenkins/Build Android")]
+    static int PerformAndroidBuild()
+    {
+        var options = CreatePlayerOptions(BuildTarget.Android);
+        var report = BuildPipeline.BuildPlayer(options);
+        var summary = report.summary;
+        if (summary.result == BuildResult.Failed)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                EditorScenes.Add(scene.path);
+            }
+        }
+        return EditorScenes.ToArray();
+    }
+
+}
