bfe58180a6fed0e7c4be71bdf45784ec0992b9b5
Owen Dugmore <oadugmore@outlook.com>
2019-11-16 17:28:01 -0800

    allow loading any scene from menu

--- Assets/Scripts/Menu/LevelLoader.cs
+++ Assets/Scripts/Menu/LevelLoader.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
+    public Text sceneText;
 
+    // Load any scene by its name.
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneText.text);
+    }
+
+    // Load a level by its number.
     public void LoadLevel(int levelNumber)
     {
         string sceneName = "Level" + levelNumber;
         SceneManager.LoadScene(sceneName);
-    }
-
-    public void InputTest()
-    {
-        SceneManager.LoadScene("InputTest");
     }
 
 }
