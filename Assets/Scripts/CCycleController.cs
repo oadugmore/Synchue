53af20a7697a34c49be10189194b9e40b1eb4819
Owen Dugmore <oadugmore@outlook.com>
2019-11-23 12:19:24 -0800

    Changed cycle controller to check player position

--- Assets/Scripts/CCycleController.cs
+++ Assets/Scripts/CCycleController.cs
@@ -4,44 +4,50 @@ using UnityEngine.Assertions;
 
 public class CCycleController : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField][Tooltip("The time in seconds it takes to complete one cycle.")]
     private float cycleTime;
-    [SerializeField]
+    [SerializeField][Tooltip("The color of cycle object to control.")]
     private InteractColor color;
-    [SerializeField]
+    [SerializeField][Tooltip("Ignore controller input outside of this range.")]
     private float leftControlBound = 15f;
-    [SerializeField]
-    private float rightControlBound = 30f;
-    [SerializeField] [Range(0f, 1f)]
+    [SerializeField][Tooltip("Ignore controller input outside of this range.")]
+    private float rightControlBound = 15f;
+    [SerializeField] [Range(0f, 1f)][Tooltip("The current percent in the cycle that this controller is at.")]
     private float cyclePosition = 0f;
 
-    private List<ICCycleObject> transformObjects;
-    private Transform cameraTransform;
+    private List<ICCycleObject> cycleObjects;
+    private Transform playerTransform;
     private float previousCyclePos = 0f;
 
     private void Start()
     {
-        cameraTransform = Camera.main.transform;
-        transformObjects = new List<ICCycleObject>(GetComponentsInChildren<ICCycleObject>());
+        playerTransform = GameObject.FindWithTag("Player").transform;
+        cycleObjects = new List<ICCycleObject>(GetComponentsInChildren<ICCycleObject>());
         Assert.AreNotEqual(cycleTime, 0f);
         UpdateObjectPositions();
     }
 
     private void FixedUpdate()
     {
-        var input = Controller.GetAxis(color);
-        cyclePosition += (Time.fixedDeltaTime * input) / cycleTime;
-        if (cyclePosition != previousCyclePos && (cameraTransform.position.x + leftControlBound) > transform.position.x && (cameraTransform.position.x - rightControlBound) < transform.position.x)
+        // Check if cyclePosition should respond to controller
+        if ((playerTransform.position.x + leftControlBound) > transform.position.x && (playerTransform.position.x - rightControlBound) < transform.position.x)
         {
-            previousCyclePos = cyclePosition;
+            var input = Controller.GetAxis(color);
+            cyclePosition += (Time.fixedDeltaTime * input) / cycleTime;
             cyclePosition = Mathf.Repeat(cyclePosition, 1);
+        }
+
+        // Check if cyclePosition changed. It could be changed by controller or through the editor
+        if (cyclePosition != previousCyclePos)
+        {
             UpdateObjectPositions();
+            previousCyclePos = cyclePosition;
         }
     }
 
     private void UpdateObjectPositions()
     {
-        foreach (var o in transformObjects)
+        foreach (var o in cycleObjects)
         {
             o.UpdateCyclePosition(cyclePosition);
         }
