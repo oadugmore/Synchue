2dd5518c9fe0bde1e90977a84ab2b8c241dc642e
Owen Dugmore <oadugmore@outlook.com>
2019-12-24 22:33:50 -0800

    added toggle capability to controller

---
+++ Assets/Scripts/CToggleController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CToggleController : MonoBehaviour
+{
+    [SerializeField][Tooltip("Whether to use toggle or hold mode.")]
+    private bool holdToActivate;
+    [SerializeField][Tooltip("The color of cycle object to control.")]
+    private InteractColor color;
+    [SerializeField][Tooltip("Ignore controller input outside of this range.")]
+    private float leftControlBound = 15f;
+    [SerializeField][Tooltip("Ignore controller input outside of this range.")]
+    private float rightControlBound = 15f;
+    [SerializeField][Tooltip("The current toggle status of the controller.")]
+    private bool on = false;
+
+    private List<ICToggleObject> toggleObjects;
+    private Transform playerTransform;
+    private bool lastButtonStatus = false;
+    private bool previousStatus = false;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+        toggleObjects = new List<ICToggleObject>(GetComponentsInChildren<ICToggleObject>());
+        UpdateObjectStates(false);
+    }
+
+    private void FixedUpdate()
+    {
+        // Check if status should respond to controller
+        if ((playerTransform.position.x + leftControlBound) > transform.position.x && (playerTransform.position.x - rightControlBound) < transform.position.x)
+        {
+            var input = Controller.GetButton(color);
+            if (holdToActivate)
+            {
+                on = input;
+            }
+            else if (lastButtonStatus != input)
+            {
+                // if this is a new button press, toggle status
+                on = input ? !on : on;
+                lastButtonStatus = input;
+            }
+            //cyclePosition += (Time.fixedDeltaTime * input) / cycleTime;
+            //cyclePosition = Mathf.Repeat(cyclePosition, 1);
+        }
+
+        // Check if status changed. It could be changed by controller or through the editor
+        if (on != previousStatus)
+        {
+            UpdateObjectStates(on);
+            previousStatus = on;
+        }
+    }
+
+    private void UpdateObjectStates(bool on)
+    {
+        foreach (var o in toggleObjects)
+        {
+            o.Toggle(on);
+        }
+    }
+}
--- Assets/Scripts/Conveyor.cs
+++ Assets/Scripts/Conveyor.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 
-public class Conveyor : MonoBehaviour
+public class Conveyor : MonoBehaviour, ICToggleObject
 {
-
     public Vector3 force;
+
+    private bool on = false;
+
+    public void Toggle(bool on)
+    {
+        this.on = on;
+        Debug.Log("Toggled " + (on ? "on" : "off"));
+    }
 
     private void OnTriggerStay(Collider other)
     {
+        if (!on) return;
         //Debug.Log("stay");
         Rigidbody r;
         if (r = other.gameObject.GetComponentInChildren<Rigidbody>())
