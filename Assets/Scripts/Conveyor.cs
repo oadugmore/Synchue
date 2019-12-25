6a6bb8a26655476d12994ef33c166e744439b350
Owen Dugmore <oadugmore@outlook.com>
2019-12-25 14:13:31 -0800

    Updated level 5 to work with new toggle controller

--- Assets/Scripts/Conveyor.cs
+++ Assets/Scripts/Conveyor.cs
@@ -9,17 +9,14 @@ public class Conveyor : MonoBehaviour, ICToggleObject
     public void Toggle(bool on)
     {
         this.on = on;
-        Debug.Log("Toggled " + (on ? "on" : "off"));
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (!on) return;
-        //Debug.Log("stay");
         Rigidbody r;
         if (r = other.gameObject.GetComponentInChildren<Rigidbody>())
         {
-            //Debug.Log("force");
             r.AddForce(force);
         }
     }
