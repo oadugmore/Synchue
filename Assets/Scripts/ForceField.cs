d45ca85826923c83dfd5c181d521b9583efc288d
Owen Dugmore <oadugmore@outlook.com>
2020-04-12 12:42:52 -0700

    add dice table and backgammon spikes

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ForceField : MonoBehaviour, ICToggleObject
+{
+    [SerializeField]
+    private Vector3 force;
+    [SerializeField]
+    private ForceMode forceMode;
+    [SerializeField]
+    private bool on;
+
+    public void Toggle(bool on)
+    {
+        this.on = on;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!on) return;
+        Rigidbody r;
+        if (r = other.gameObject.GetComponentInParent<Rigidbody>())
+        {
+            r.AddForce(force, forceMode);
+        }
+    }
+}
