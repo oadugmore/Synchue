954031479e862352a2b8915d2dc4aca3148b5e81
Owen Dugmore <oadugmore@outlook.com>
2020-05-14 01:31:46 -0700

    add initial implementation of win screen

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void NextLevel()
+    {
+        
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
