4985d28bf350ec0fe0e14e2c2b66bcc4970f4d3d
Owen Dugmore <oadugmore@outlook.com>
2020-12-28 16:06:03 -0800

    add core haptics support

--- Assets/Scripts/MobileUtils.cs
+++ Assets/Scripts/MobileUtils.cs
@@ -1,26 +1,37 @@
 using UnityEngine;
+using UnityCoreHaptics;
 
 public class MobileUtils
 {
     private static AndroidJavaObject androidVibrator = null;
     private static AndroidJavaClass vibrationEffectClass = null;
-    private static bool initialized = false;
-    private static int androidApiVersion = 1;
-    private static bool androidRequiresLegacyApi = false;
+    private static bool vibratorInitialized = false;
+    private static bool androidRequiresLegacyVibratorApi = false;
+    private static bool iPhoneSupportsCoreHaptics = false;
     private static bool hasVibrator = false;
+
+    public static int GetAndroidApiVersion()
+    {
+        var version = -1;
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            using (AndroidJavaClass androidVersionClass = new AndroidJavaClass("android.os.Build$VERSION"))
+            {
+                version = androidVersionClass.GetStatic<int>("SDK_INT");
+            }
+        }
+        return version;
+    }
 
     public static void InitializeVibrator()
     {
         if (Application.platform == RuntimePlatform.Android)
         {
-            using (AndroidJavaClass androidVersionClass = new AndroidJavaClass("android.os.Build$VERSION"))
+            if (GetAndroidApiVersion() < 26)
             {
-                androidApiVersion = androidVersionClass.GetStatic<int>("SDK_INT");
-                if (androidApiVersion < 26)
-                {
-                    androidRequiresLegacyApi = true;
-                }
+                androidRequiresLegacyVibratorApi = true;
             }
+
             using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
             using (AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
             {
@@ -28,14 +39,28 @@ public class MobileUtils
                 {
                     androidVibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
                     hasVibrator = androidVibrator != null && androidVibrator.Call<bool>("hasVibrator");
-                    if (!androidRequiresLegacyApi)
+                    if (!androidRequiresLegacyVibratorApi)
                     {
                         vibrationEffectClass = new AndroidJavaClass("android.os.VibrationEffect");
                     }
                 }
             }
         }
-        initialized = true;
+        else if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            if (UnityCoreHapticsProxy.SupportsCoreHaptics())
+            {
+                iPhoneSupportsCoreHaptics = true;
+                UnityCoreHapticsProxy.CreateEngine();
+                UnityCoreHapticsProxy.OnEngineCreated += () => { Debug.Log("Created engine"); };
+                UnityCoreHapticsProxy.OnEngineError += () => { Debug.LogError("UnityCoreHaptics Engine had an error."); };
+            }
+            else
+            {
+                Debug.Log("This iPhone does not support Core Haptics.");
+            }
+        }
+        vibratorInitialized = true;
     }
 
     /// <summary>
@@ -43,14 +68,44 @@ public class MobileUtils
     /// </summary>
     /// <param name="duration">The duration in seconds.</param>
     /// <param name="intensity">The intensity as a percent.</param>
-    public static void Vibrate(float duration, float intensity)
+    /// <param name="sharpness">The sharpness as a percent. Only affects iPhone with Core Haptics.</param>
+    public static void Vibrate(float duration, float intensity, float sharpness)
     {
         if (!hasVibrator) return;
-        if (!initialized) InitializeVibrator();
+        if (!vibratorInitialized)
+        {
+            Debug.LogWarning("Vibrate was called without first initializing the vibrator.");
+            InitializeVibrator();
+        }
         intensity = Mathf.Clamp01(intensity);
         if (Application.platform == RuntimePlatform.Android)
         {
             VibrateAndroid((long)(duration * 1000), (int)(intensity * 255));
+        }
+        else if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            VibrateiPhone(duration, intensity, sharpness);
+        }
+        else
+        {
+            Handheld.Vibrate();
+        }
+    }
+
+    private static void VibrateiPhone(float duration, float intensity, float sharpness)
+    {
+        if (iPhoneSupportsCoreHaptics)
+        {
+            if (duration > 0)
+            {
+                UnityCoreHapticsProxy.PlayContinuousHaptics(intensity, sharpness, duration);
+                Debug.Log("played continuous haptics");
+            }
+            else
+            {
+                UnityCoreHapticsProxy.PlayTransientHaptics(intensity, sharpness);
+                Debug.Log("played transient haptics");
+            }
         }
         else
         {
@@ -60,7 +115,7 @@ public class MobileUtils
 
     private static void VibrateAndroid(long milliseconds, int amplitude)
     {
-        if (androidRequiresLegacyApi)
+        if (androidRequiresLegacyVibratorApi)
         {
             androidVibrator.Call("vibrate", milliseconds);
         }
