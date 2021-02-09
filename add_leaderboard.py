4f7f66544a82c80550b48fa0c9ab0d6914b6f457
Owen Dugmore <oadugmore@outlook.com>
2021-02-09 01:21:11 -0800

    improve script

--- add_leaderboard.py
+++ add_leaderboard.py
@@ -13,7 +13,7 @@ GOOGLE_PLAY_APP_ID = '711191715930'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('name', help='the friendly name of the leaderboard')
-# parser.add_argument('ios_id', help='the Game Center ID of the leaderboard')
+parser.add_argument('ios_id', help='the Game Center ID of the leaderboard')
 args = parser.parse_args()
 
 # load_dotenv()
@@ -23,96 +23,47 @@ if (not os.path.isfile('client_secret.json')):
     sys.exit(
         f'Must configure a client_secret.json')
 
-google_play_data = {
-    "kind": "gamesConfiguration#leaderboardConfiguration",
-    #   "token": string,
-    #   "id": string,
-    "scoreOrder": 'SMALLER_IS_BETTER',
-    #   "scoreMin": long,
-    #   "scoreMax": long,
-    "draft": {
-        "kind": "gamesConfiguration#leaderboardConfigurationDetail",
-        "name": {
-            "kind": "gamesConfiguration#localizedStringBundle",
-            "translations": [
-                {
-                    "kind": "gamesConfiguration#localizedString",
-                    "locale": 'en_US',
-                    "value": args.name
-                }
-            ]
-        },
-        # "iconUrl": string,
-        # "sortRank": integer,
-        "scoreFormat": {
-            "numberFormatType": 'TIME_DURATION',
-            #   "suffix": {
-            #     "zero": {
-            #       "kind": "gamesConfiguration#localizedStringBundle",
-            #       "translations": [
-            #         {
-            #           "kind": "gamesConfiguration#localizedString",
-            #           "locale": string,
-            #           "value": string
-            #         }
-            #       ]
-            #     },
-            #     "one": {
-            #       "kind": "gamesConfiguration#localizedStringBundle",
-            #       "translations": [
-            #         {
-            #           "kind": "gamesConfiguration#localizedString",
-            #           "locale": string,
-            #           "value": string
-            #         }
-            #       ]
-            #     },
-            #     "two": {
-            #       "kind": "gamesConfiguration#localizedStringBundle",
-            #       "translations": [
-            #         {
-            #           "kind": "gamesConfiguration#localizedString",
-            #           "locale": string,
-            #           "value": string
-            #         }
-            #       ]
-            #     },
-            #     "few": {
-            #       "kind": "gamesConfiguration#localizedStringBundle",
-            #       "translations": [
-            #         {
-            #           "kind": "gamesConfiguration#localizedString",
-            #           "locale": string,
-            #           "value": string
-            #         }
-            #       ]
-            #     },
-            #     "many": {
-            #       "kind": "gamesConfiguration#localizedStringBundle",
-            #       "translations": [
-            #         {
-            #           "kind": "gamesConfiguration#localizedString",
-            #           "locale": string,
-            #           "value": string
-            #         }
-            #       ]
-            #     },
-            #     "other": {
-            #       "kind": "gamesConfiguration#localizedStringBundle",
-            #       "translations": [
-            #         {
-            #           "kind": "gamesConfiguration#localizedString",
-            #           "locale": string,
-            #           "value": string
-            #         }
-            #       ]
-            #     }
-            #   },
-            #   "numDecimalPlaces": integer,
-            #   "currencyCode": string
+
+def create_leaderboard(credentials, name):
+    google_play_data = {
+        "kind": "gamesConfiguration#leaderboardConfiguration",
+        #   "token": string,
+        #   "id": string,
+        "scoreOrder": 'SMALLER_IS_BETTER',
+        #   "scoreMin": long,
+        #   "scoreMax": long,
+        "draft": {
+            "kind": "gamesConfiguration#leaderboardConfigurationDetail",
+            "name": {
+                "kind": "gamesConfiguration#localizedStringBundle",
+                "translations": [
+                    {
+                        "kind": "gamesConfiguration#localizedString",
+                        "locale": 'en-US',
+                        "value": name
+                    }
+                ]
+            },
+            # "iconUrl": string,
+            # "sortRank": integer,
+            "scoreFormat": {
+                "numberFormatType": 'TIME_DURATION',
+            }
         }
     }
-}
+
+    with build('gamesConfiguration', 'v1configuration', credentials=credentials) as leaderboard_service:
+        print('Calling Google Play API')
+        configurations = leaderboard_service.leaderboardConfigurations()
+        request = configurations.insert(
+            applicationId=GOOGLE_PLAY_APP_ID, body=google_play_data)
+        try:
+            response = request.execute()
+        except HttpError as e:
+            print(
+                f'Error response status code: {e.resp.status}, reason: {e.error_details}')
+            print(f'Details: {e.content}')
+
 
 flow = InstalledAppFlow.from_client_secrets_file(
     'client_secret.json',
@@ -124,13 +75,11 @@ credentials = flow.run_local_server(host='localhost',
                                     success_message='The auth flow is complete; you may close this window.',
                                     open_browser=True)
 
-with build('gamesConfiguration', 'v1configuration', credentials=credentials) as leaderboard_service:
-    print('Calling Google Play API')
-    configurations = leaderboard_service.leaderboardConfigurations()
-    request = configurations.insert(
-        applicationId=GOOGLE_PLAY_APP_ID, body=google_play_data)
-    try:
-        response = request.execute()
-    except HttpError as e:
-        print('Error response status code : {0}, reason : {1}'.format(
-            e.resp.status, e.error_details))
+# for world in range(1, 3):
+#     for level in range(1, 9):
+#         if world == 1 and (level == 1 or level == 2):
+#             print(f'skipping world {world} level {level}')
+#             continue
+#         print(f'adding world {world} level {level}')
+#         create_leaderboard(credentials=credentials,
+#                            name=f'Level {world}-{level}')
