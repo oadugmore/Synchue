6ff7143edab82beb06e79def2e5a3e58f158123e
Owen Dugmore <oadugmore@outlook.com>
2021-02-24 21:43:06 -0800

    add splash screen

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    public float fadeTime = 1f;
+    public float waitTime = 2f;
+    public CanvasGroup textCanvasGroup;
+    public CanvasGroup backgroundCanvasGroup;
+
+    void Start()
+    {
+        backgroundCanvasGroup.alpha = 1f;
+        textCanvasGroup.alpha = 0f;
+        LeanTween.alphaCanvas(textCanvasGroup, 1, fadeTime)
+            .setOnComplete(() => Invoke(nameof(FadeOut), waitTime));
+    }
+
+    void FadeOut()
+    {
+        LeanTween.alphaCanvas(backgroundCanvasGroup, 0, fadeTime)
+            .setDestroyOnComplete(true);
+    }
+}
