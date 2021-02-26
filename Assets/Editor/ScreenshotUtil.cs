a8603e563568e884a715b80f4bafc853cf1bdea0
Owen Dugmore <oadugmore@outlook.com>
2021-02-25 19:42:00 -0800

    add screenshotutil

---
+++ Assets/Editor/ScreenshotUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ScreenshotUtil : MonoBehaviour
+{
+    public class TakeScreenshot
+{
+ 
+    [MenuItem("Tools/Take Screenshot")]
+    static public void OnTakeScreenshot()
+    {
+        // Application.CaptureScreenshot(EditorUtility.SaveFilePanel("Save Screenshot As", "", "", "png"));
+        ScreenCapture.CaptureScreenshot(EditorUtility.SaveFilePanel("Save Screenshot As", "", "", "png"));
+    }
+}
+
+}
