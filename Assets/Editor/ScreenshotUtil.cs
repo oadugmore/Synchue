dd199a9a349459f38fa9d280ef46d9f38c5fcb17
Owen Dugmore <oadugmore@outlook.com>
2021-02-25 21:44:31 -0800

    implement screenshot util

--- Assets/Editor/ScreenshotUtil.cs
+++ Assets/Editor/ScreenshotUtil.cs
@@ -1,19 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
+using Unity.EditorCoroutines.Editor;
 
-public class ScreenshotUtil : MonoBehaviour
+public class ScreenshotUtil : EditorWindow
 {
-    public class TakeScreenshot
-{
- 
-    [MenuItem("Tools/Take Screenshot")]
-    static public void OnTakeScreenshot()
+    string screenshotDir;
+    int screenshotNumber;
+    int screenshotScale = 1;
+    string prefix = "Editor";
+    // [MenuItem("Tools/Take Screenshot")]
+    [MenuItem("Window/Screenshot Utility")]
+    static public void ShowScreenshotUtilWindow()
     {
         // Application.CaptureScreenshot(EditorUtility.SaveFilePanel("Save Screenshot As", "", "", "png"));
-        ScreenCapture.CaptureScreenshot(EditorUtility.SaveFilePanel("Save Screenshot As", "", "", "png"));
+        
+        var util = GetWindow<ScreenshotUtil>("Screenshot Utility");
+        util.screenshotDir = System.IO.Directory.GetCurrentDirectory();
+        util.screenshotNumber = 1;
+        // window.position = new Rect(window.position.xMin + 100f, window.position.yMin + 100f, 200f, 400f);
     }
-}
+    public void CaptureScreenshot()
+    {
+        var outputPath = Path.Combine(screenshotDir, $"{prefix} {Screen.currentResolution.width}x{Screen.currentResolution.height} {screenshotNumber}.png");
+        ScreenCapture.CaptureScreenshot(outputPath, screenshotScale);
+    }
+
+    public IEnumerator TakeAppStoreScreenshots()
+    {
+        var oldResolution = Screen.currentResolution;
+
+        // iPad Pro 3rd gen (2019) 12.9" (also accepted for iPad Pro 2nd gen)
+        Screen.SetResolution(2732, 2048, false);
+        CaptureScreenshot();
+        yield return new WaitForEndOfFrame();
+
+        // iPhone 5.5"
+        Screen.SetResolution(2208, 1242, false);
+        CaptureScreenshot();
+        yield return new WaitForEndOfFrame();
+
+        // iPhone 6.5"
+        Screen.SetResolution(2688, 1242, false);
+        CaptureScreenshot();
+        yield return new WaitForEndOfFrame();
+
+        Screen.SetResolution(oldResolution.width, oldResolution.height, false);
+    }
+
+    private void OnGUI()
+    {
+        screenshotDir = EditorGUILayout.TextField("Base directory", screenshotDir);
+        screenshotNumber = EditorGUILayout.IntField("Number", screenshotNumber);
+        screenshotScale = EditorGUILayout.IntField("Scale", screenshotScale);
+        // if (GUILayout.Button("Take screenshot"))
+        // {
+        //     CaptureScreenshot();
+        //     screenshotNumber++;
+        // }
+        if (GUILayout.Button("Take all App Store screenshots"))
+        {
+            EditorCoroutineUtility.StartCoroutine(TakeAppStoreScreenshots(), this);
+        }
+    }
+
 
 }
